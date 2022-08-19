80f01822d34d7d484db3d5d674dddb0a7f51b783
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2022-08-18 21:41:34 -0300

    modified:   pilhas/Testes.java

--- pilhas/Testes.java
+++ pilhas/Testes.java
@@ -8,40 +8,49 @@ public class Testes {
      */
     public static void main(String[] args) {
         
-        //Instanciando a pilha em um tamanho definido |
-        Pilha pilha = new Pilha();
+        Pilha pilha01 = new Pilha();
 
         //Utilizando o método push para inserir elementos na pilha |
-        pilha.Push(2);
-        pilha.Push(4);
-        pilha.Push(5);
-        pilha.Push(7);
-        pilha.Push(8);
-        pilha.Push(10);
-        pilha.Push(12);
-        pilha.Push(16);
-        pilha.Push(20);
-        pilha.Push(17);
-        
-        //Imprimindo a pilha |
-        pilha.Imprimir();
-        
-        //Testando inserir em pilha "cheia" |
-        pilha.Push(51);
-        pilha.Push(25);
-        pilha.Push(18);
-        pilha.Push(33);
-        pilha.Push(50);
-        pilha.Push(65);
+        pilha01.Push(2);
+        pilha01.Push(4);
+        pilha01.Push(5);
+        pilha01.Push(7);
+        pilha01.Push(8);
+        pilha01.Push(10);
 
-        pilha.Imprimir();
+        //Imprimindo a pilha |
+        pilha01.Imprimir();
 
         //Utilizando o método pop para remoção do elemento no topo da pilha |
-        pilha.Pop();
-        pilha.Pop();
-        pilha.Pop();
+        pilha01.Pop();
+        pilha01.Pop();
+        pilha01.Pop();
+        pilha01.Pop();
 
-        pilha.Imprimir();
+        pilha01.Imprimir();
+        
+        //Instanciando a pilha em um tamanho definido |
+        Pilha pilha02 = new Pilha(5);
+
+        pilha02.Push(7);
+        pilha02.Push(13);
+        pilha02.Push(47);
+        pilha02.Push(16);
+        pilha02.Push(52);
+
+        pilha02.Imprimir();
+
+        //Testando inserir em pilha "cheia" |
+        pilha02.Push(51);
+        pilha02.Push(25);
+        pilha02.Push(18);
+
+        pilha02.Imprimir();
+
+        pilha02.Pop();
+        pilha02.Pop();
+
+        pilha02.Imprimir();
         
     }    
 }
