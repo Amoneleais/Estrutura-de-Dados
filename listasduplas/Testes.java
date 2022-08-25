2b1a39c23b695839aa2e98caea77405c26e9dfce
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2022-08-25 19:23:57 -0300

    modified:   listasduplas/Testes.java

--- listasduplas/Testes.java
+++ listasduplas/Testes.java
@@ -12,7 +12,7 @@ public class Testes {
 
         //1 - Lista Vazia |
  
-        System.out.println("\nLista Vazia: "+Lista.ListaVazia());
+        System.out.println("\nLista Vazia: "+Lista.ListaVazia());//true
 
         //2 - Utilizando Inserção |
 
@@ -30,34 +30,34 @@ public class Testes {
 
         //3 - Utilizando Impressão |
 
-        Lista.Imprimir();
+        Lista.Imprimir();//80,30,70,2590,55,9,1,1,5,20,10
 
         //4 - Utilizando Remoção de Elementos Existentes/Inexistentes |
 
-        Lista.Remover(32);
+        Lista.Remover(32);//Inexistente
         Lista.Remover(30);
         Lista.Remover(1);
         Lista.Remover(20);
-        Lista.Remover(20);
+        Lista.Remover(20);//Inexistente
         Lista.Remover(10);
 
-        Lista.Imprimir();
+        Lista.Imprimir();//80,70,2590,55,9,1,5
 
         //5 - Utilizando Busca |
 
-        Lista.Buscar(200);
-        Lista.Buscar(55);
-        Lista.Buscar(20);
+        Lista.Buscar(200);//Inexistente
+        Lista.Buscar(55);//Anterior: 2590, Elemento: 55, Prox: 9, Pos: 4
+        Lista.Buscar(20);//Inexistente
 
         //6 - Lista Vazia depois de Inserções |
 
-        System.out.println("\nLista Vazia: "+Lista.ListaVazia());
+        System.out.println("\nLista Vazia: "+Lista.ListaVazia());//false
 
         //7 - Esvaziando Lista |
 
         Lista.Esvaziar();
 
-        Lista.Imprimir();
+        Lista.Imprimir();//Lista Vazia
 
         //8 - Inserções após esvaziar |
 
@@ -65,7 +65,7 @@ public class Testes {
         Lista.Inserir(20);
         Lista.Inserir(5);
 
-        Lista.Imprimir();
+        Lista.Imprimir();//10,20,5
 
         //9 - Removendo Todos os elementos um por um |
   
@@ -73,7 +73,7 @@ public class Testes {
         Lista.Remover(20);
         Lista.Remover(5);
 
-        Lista.Imprimir();
+        Lista.Imprimir();//Lista Vazia
 
     }
 }
