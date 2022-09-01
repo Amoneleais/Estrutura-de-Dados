40951c7b561406f67d9e265c10a41a4de8c1e6c5
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2022-08-31 21:37:26 -0300

    modified:   listasduplas/ListaEncadeadaDupla.java

--- listasduplas/ListaEncadeadaDupla.java
+++ listasduplas/ListaEncadeadaDupla.java
@@ -32,46 +32,53 @@ public class ListaEncadeadaDupla {
     public void remover(long elemento){
         No anterior = null;
         if(listaVazia()){
-            System.out.println("\n [ REMOVER: LISTA VAZIA! ]");
+            try {
+                throw new Exception("[ REMOVER: LISTA VAZIA! ]");
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
         }
-        else{
-            //percorre os nós, por meio de uma variável auxiliar do tipo No até encontrar o nulo.
-            for(No auxiliar = this.ref; auxiliar != null; auxiliar = auxiliar.getProx()){
-                //em caso de elemento inexistente.
-                if(auxiliar.getProx() == null && auxiliar.getInfo() != elemento){
-                    System.out.println("\n [ REMOVER: ELEMENTO NÃO EXISTENTE! ]");
+        //percorre os nós, por meio de uma variável auxiliar do tipo No até encontrar o nulo.
+        for(No auxiliar = this.ref; auxiliar != null; auxiliar = auxiliar.getProx()){
+            //em caso de elemento inexistente.
+            if(auxiliar.getProx() == null && auxiliar.getInfo() != elemento){
+                try {
+                    throw new Exception("[ REMOVER: ELEMENTO NÃO EXISTENTE! ]");
+                } catch (Exception e) {
+                    e.printStackTrace();
                     break;
                 }
-                //condicional, caso a info do nó atual ser igual ao elemento a ser removido.
-                else if(auxiliar.getInfo() == elemento){    
-                    //caso o elemento seja a referência e haja sucessor.
-                    if(auxiliar == this.ref & this.ref.getProx() != null){
-                        ref = auxiliar.getProx();
-                        ref.setAnterior(null);
-                        this.tamanho--;
-                        System.out.println("\n - "+elemento+" REMOVIDO COM SUCESSO!");
-                        break;
+            }
+            //condicional, caso a info do nó atual ser igual ao elemento a ser removido.
+            else if(auxiliar.getInfo() == elemento){    
+                //caso o elemento seja a referência e haja sucessor.
+                if(auxiliar == this.ref & this.ref.getProx() != null){
+                    ref = auxiliar.getProx();
+                    ref.setAnterior(null);
+                    this.tamanho--;
+                    System.out.println("\n - "+elemento+" REMOVIDO COM SUCESSO!\n");
+                    break;
+                }
+                //caso o elemento seja a referência e não haja nenhum elemento sucessor.
+                else if(auxiliar == this.ref){
+                    this.ref = null;
+                        System.out.println("\n - "+elemento+" REMOVIDO COM SUCESSO!\n");
+                    this.tamanho--;
+                    break;
+                }
+                //caso não seja a referência.
+                else {
+                    anterior.setProx(auxiliar.getProx());
+                    if(anterior.getProx() != null){
+                        anterior.getProx().setAnterior(anterior);
                     }
-                    //caso o elemento seja a referência e não haja nenhum elemento sucessor.
-                    else if(auxiliar == this.ref){
-                        this.ref = null;
-                        System.out.println("\n - "+elemento+" REMOVIDO COM SUCESSO!");
-                        this.tamanho--;
-                        break;
-                    }
-                    //caso não seja a referência.
-                    else {
-                        anterior.setProx(auxiliar.getProx());
-                        if(anterior.getProx() != null){
-                            anterior.getProx().setAnterior(anterior);
-                        }
-                        System.out.println("\n - "+elemento+" REMOVIDO COM SUCESSO!");
-                        this.tamanho--;
-                        break;
+                    System.out.println("\n - "+elemento+" REMOVIDO COM SUCESSO!\n");
+                    this.tamanho--;
+                    break;
                     }
                 }
-                anterior = auxiliar;
-            }
+            anterior = auxiliar;
         }
     }
 
@@ -80,38 +87,46 @@ public class ListaEncadeadaDupla {
         //transforma a referência em nulo, apagando os nós sucessores.
         this.ref = null;
         this.tamanho = 0;
-        System.out.println("\n - A LISTA FOI ESVAZIADA");
+        System.out.println("\n - A LISTA FOI ESVAZIADA\n");
     }
     
     // * Buscar Elemento na Lista Dupla |
     public void buscar(long elemento){
         int posicao = 0;
         if(listaVazia()){
-            System.out.println("\n [ BUSCAR: LISTA VAZIA! ]");
+            try {
+                throw new Exception("[ BUSCAR: LISTA VAZIA! ]");
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
         }
         //percorre os nós, por meio de uma variável auxiliar do tipo No até encontrar o nulo.
-        else{
-            for(No auxiliar = this.ref; auxiliar != null; auxiliar = auxiliar.getProx()){
-                posicao++;
-                //caso a informação do No seja igual ao elemento, retorna para o usuário o elemento, posição, anterior e sucessor.
-                if(auxiliar.getInfo() == elemento){
-                    if(auxiliar.getProx() == null & auxiliar.getAnterior() != null){
-                        System.out.println("\n ANTERIOR: ["+auxiliar.getAnterior().getInfo()+"]\n ELEMENTO: ["+auxiliar.getInfo()+"]\n PROXIMO:  ["+null+"]"+"\n POSICAO:   "+posicao);
-                    }
-                    else if(auxiliar.getAnterior() == null & auxiliar.getProx() != null){
-                        System.out.println("\n ANTERIOR: ["+null+"]\n ELEMENTO: ["+auxiliar.getInfo()+"]\n PROXIMO:  ["+auxiliar.getProx().getInfo()+"]"+"\n POSICAO:   "+posicao);
-                    }
-                    else if(auxiliar.getAnterior() == null & auxiliar.getProx() == null){
-                        System.out.println("\n ANTERIOR: ["+null+"]\n ELEMENTO: ["+auxiliar.getInfo()+"]\n PROXIMO:  ["+null+"]"+"\n POSICAO:   "+posicao);
-                    }
-                    else{
-                        System.out.println("\n ANTERIOR: ["+auxiliar.getAnterior().getInfo()+"]\n ELEMENTO: ["+auxiliar.getInfo()+"]\n PROXIMO:  ["+auxiliar.getProx().getInfo()+"]"+"\n POSICAO:   "+posicao);
-                    }
-                    break;
+        for(No auxiliar = this.ref; auxiliar != null; auxiliar = auxiliar.getProx()){
+            posicao++;
+            //caso a informação do No seja igual ao elemento, retorna para o usuário o elemento, posição, anterior e sucessor.
+            if(auxiliar.getInfo() == elemento){
+                if(auxiliar.getProx() == null & auxiliar.getAnterior() != null){
+                    System.out.println("\n ANTERIOR: ["+auxiliar.getAnterior().getInfo()+"]\n ELEMENTO: ["+auxiliar.getInfo()+"]\n PROXIMO:  ["+null+"]"+"\n POSICAO:   "+posicao+"\n");
                 }
-                //caso o elemento não esteja inserido na lista, retorna mensagem para o usuário.
-                else if(auxiliar.getProx() == null & auxiliar.getInfo() != elemento){
-                    System.out.println("\n [ BUSCAR: ELEMENTO NÃO EXISTENTE! ]");
+                else if(auxiliar.getAnterior() == null & auxiliar.getProx() != null){
+                    System.out.println("\n ANTERIOR: ["+null+"]\n ELEMENTO: ["+auxiliar.getInfo()+"]\n PROXIMO:  ["+auxiliar.getProx().getInfo()+"]"+"\n POSICAO:   "+posicao+"\n");
+                }
+                else if(auxiliar.getAnterior() == null & auxiliar.getProx() == null){
+                    System.out.println("\n ANTERIOR: ["+null+"]\n ELEMENTO: ["+auxiliar.getInfo()+"]\n PROXIMO:  ["+null+"]"+"\n POSICAO:   "+posicao+"\n");
+                }
+                else{
+                    System.out.println("\n ANTERIOR: ["+auxiliar.getAnterior().getInfo()+"]\n ELEMENTO: ["+auxiliar.getInfo()+"]\n PROXIMO:  ["+auxiliar.getProx().getInfo()+"]"+"\n POSICAO:   "+posicao+"\n");
+                }
+                break;
+            }
+            //caso o elemento não esteja inserido na lista, retorna mensagem para o usuário.
+            else if(auxiliar.getProx() == null & auxiliar.getInfo() != elemento){
+                try {
+                    throw new Exception("[ BUSCAR: ELEMENTO NÃO EXISTENTE! ]");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return;
                 }
             }
         }
@@ -123,33 +138,35 @@ public class ListaEncadeadaDupla {
         String listaVolta = " ";
         //retorna mensagem para o usuário, em caso de lista vazia.
         if(listaVazia()){
-            System.out.println("\n [ IMPRIMIR: LISTA VAZIA! ]");
+            try {
+                throw new Exception("[ IMPRIMIR: LISTA VAZIA! ]");
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
         }
-        else{
-            //percorre os nós, por meio de uma variável auxiliar do tipo No até encontrar o nulo.
-            for(No auxiliarVai = this.ref; auxiliarVai != null; auxiliarVai = auxiliarVai.getProx()){
-                //caso a próxima variável seja nula irá executar o outro laço
-                if(auxiliarVai.getProx() == null){
-                    listaVai += "["+auxiliarVai.getInfo()+"]";
-                    //percorre os nós no sentido contrário, do fim ao início, por meio da busca pelo anterior.
-                    for(No auxiliarVolta = auxiliarVai; auxiliarVolta != null; auxiliarVolta = auxiliarVolta.getAnterior()){
-                        if(auxiliarVolta.getAnterior() !=  null){
-                            listaVolta += "["+auxiliarVolta.getInfo()+"] <- ";
-                        }
-                        else{
-                            listaVolta += "["+auxiliarVolta.getInfo()+"]";
-                        }
+        //percorre os nós, por meio de uma variável auxiliar do tipo No até encontrar o nulo.
+        for(No auxiliarVai = this.ref; auxiliarVai != null; auxiliarVai = auxiliarVai.getProx()){
+            //caso a próxima variável seja nula irá executar o outro laço
+            if(auxiliarVai.getProx() == null){
+                listaVai += "["+auxiliarVai.getInfo()+"]";
+                //percorre os nós no sentido contrário, do fim ao início, por meio da busca pelo anterior.
+                for(No auxiliarVolta = auxiliarVai; auxiliarVolta != null; auxiliarVolta = auxiliarVolta.getAnterior()){
+                    if(auxiliarVolta.getAnterior() !=  null){
+                        listaVolta += "["+auxiliarVolta.getInfo()+"] <- ";
+                    }
+                    else{
+                        listaVolta += "["+auxiliarVolta.getInfo()+"]";
                     }
                 }
-                else{
-                    listaVai += "["+auxiliarVai.getInfo()+"] -> ";
-                }
+                return;
             }
-            //impressão das variáveis com todos os elementos inseridos, em ambos os sentidos.
-            System.out.println("\n"+listaVai);
-            System.out.println("\n IMPRESSÃO NO SENTIDO CONTRÁRIO [FIM -> INICIO]:\n\n"+listaVolta);
-            System.out.println(" - Tamanho da Lista: "+this.tamanho);
+            listaVai += "["+auxiliarVai.getInfo()+"] -> ";
         }
+        //impressão das variáveis com todos os elementos inseridos, em ambos os sentidos.
+        System.out.println("\n"+listaVai);
+        System.out.println("\n IMPRESSÃO NO SENTIDO CONTRÁRIO [FIM -> INICIO]:\n\n"+listaVolta);
+        System.out.println(" - Tamanho da Lista: "+this.tamanho+"\n");
     }
 
 }
