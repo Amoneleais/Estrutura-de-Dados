d55242b8f653c1eaa7abce2c40de6d341d31bb54
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2022-09-02 11:38:36 -0300

    modified:   fila/Testes.java 	modified:   pilhas/Testes.java

--- fila/Testes.java
+++ fila/Testes.java
@@ -8,18 +8,22 @@ public class Testes {
         Random random = new Random();
         Fila fila = new Fila();
         
+        // * Utilizando o método enqueue para inserir elementos na fila | 
         fila.enqueue(random.nextLong(1000));
         fila.enqueue(random.nextLong(1000));
         fila.enqueue(random.nextLong(1000));
         
+        // * Imprimindo a fila com o método imprimir |
         fila.imprimir();
 
+        // * Removendo o elemento mais antigo da fila com o método dequeue |
         fila.dequeue();
         fila.dequeue();
         fila.dequeue();
         
         fila.imprimir();
 
+        // * Inserção após esvaziar a pilha |
         fila.enqueue(random.nextLong(1000));
         fila.enqueue(random.nextLong(1000));
         fila.enqueue(random.nextLong(1000));
--- pilhas/Testes.java
+++ pilhas/Testes.java
@@ -1,4 +1,7 @@
 package pilhas;
+
+import java.util.Random;
+
 /**
  * Testes
  */
@@ -10,6 +13,7 @@ public class Testes {
         
 
         Pilha pilha01 = new Pilha();
+        Random random = new Random();
         
         System.out.println("\n");
         
@@ -17,39 +21,39 @@ public class Testes {
         pilha01.imprimir(); // ! Excessão
 
         // * Utilizando o método push para inserir elementos na pilha |
-        pilha01.push(2);
-        pilha01.push(4);
-        pilha01.push(5);
-        pilha01.push(7);
-        pilha01.push(8);
-        pilha01.push(10);
+        pilha01.push(random.nextLong(1000));
+        pilha01.push(random.nextLong(1000));
+        pilha01.push(random.nextLong(1000));
+        pilha01.push(random.nextLong(1000));
+        pilha01.push(random.nextLong(1000));
+        pilha01.push(random.nextLong(1000));
         
         // * Imprimindo a pilha |
         pilha01.imprimir();
         
         // * Utilizando o método pop para remoção do elemento no topo da pilha |
-        pilha01.pop();
-        pilha01.pop();
-        pilha01.pop();
-        pilha01.pop();
+        pilha01.push(random.nextLong(1000));
+        pilha01.push(random.nextLong(1000));
+        pilha01.push(random.nextLong(1000));
+        pilha01.push(random.nextLong(1000));
         
         pilha01.imprimir();
         
         // * Instanciando a pilha em um tamanho definido |
         Pilha pilha02 = new Pilha(5);
         
-        pilha02.push(7);
-        pilha02.push(13);
-        pilha02.push(47);
-        pilha02.push(16);
-        pilha02.push(52);
+        pilha02.push(random.nextLong(1000));
+        pilha02.push(random.nextLong(1000));
+        pilha02.push(random.nextLong(1000));
+        pilha02.push(random.nextLong(1000));
+        pilha02.push(random.nextLong(1000));
 
         pilha02.imprimir();
 
         // ! Inserção em pilha "cheia" |
-        pilha02.push(51); // ! Excessão
-        pilha02.push(25); // ! Excessão
-        pilha02.push(18); // ! Excessão
+        pilha02.push(random.nextLong(1000)); // ! Excessão
+        pilha02.push(random.nextLong(1000)); // ! Excessão
+        pilha02.push(random.nextLong(1000)); // ! Excessão
         
         pilha02.imprimir();
         
